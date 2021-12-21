48887ad775eaca12615ef35ea163a8f827e11563
Tuama <lesser.anteater@gmail.com>
2021-12-21 14:39:29 +0200

    An async solution with a callback

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const fs = require("fs");
+
 const datafile = "server/data/clothing.json";
+
 const router = express.Router();
 
 /* GET all clothing */
 router.route("/")
   .get(function(req, res) {
-
-    fs.readFile(datafile, "utf8", (err, data) => {
+    getClothingData((err, data) => {
       if (err) {
         console.log(err);
       } else {
-        let clothingData = JSON.parse(data);
         console.log("Returning clothing data");
-        res.send(clothingData);
+        res.send(data);
       }
     });
-
     console.log("Doing more work");
   });
 
+function getClothingData(callback) {
+  fs.readFile(datafile, "utf8", (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let clothingData = JSON.parse(data);
+      callback(null, clothingData);
+    }
+  });
+}
 module.exports = router;
