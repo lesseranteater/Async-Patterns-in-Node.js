a0dfb0c755872c43ae423c80aad0b73fb597c748
alex <alex.iconia.tab@gmail.com>
2021-12-21 22:11:02 +0200

    Event emitter

--- event_emitter.js
+++ event_emitter.js
@@ -1,19 +1,19 @@
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 
 let myEventEmitter = new EventEmitter();
-myEventEmitter.on("wroteCode", () =>{
-    console.log("Somebody wrote some code!")
-})
+myEventEmitter.on("wroteCode", () => {
+  console.log("Somebody wrote some code!");
+});
 
-myEventEmitter.on("wroteCode", () =>{
-    console.log("Busy building Node apps!")
-})
+myEventEmitter.on("wroteCode", () => {
+  console.log("Busy building Node apps!");
+});
 
 myEventEmitter.emit("wroteCode");
 
 let paramEventEmitter = new EventEmitter();
 paramEventEmitter.on("wroteCode", (language) => {
-    console.log(`Somebody wrote some ${language} code!`);
-})
+  console.log(`Somebody wrote some ${language} code!`);
+});
 
-paramEventEmitter.emit("wroteCode","Java");
+paramEventEmitter.emit("wroteCode", "Java");
--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,52 +1,75 @@
 const express = require("express");
 const fs = require("fs");
-const { colors } = require("debug");
 const fsPromises = require("fs").promises;
-
 const datafile = "server/data/clothing.json";
-
 const router = express.Router();
 
-/* GET all clothing */
-router.route("/")
-  .get(async function(req, res) {
-    try{
-      let data = await getClothingData();
-      console.log("Returning async data");
-      res.send(data);
-    } catch(error){
-      res.status(500).send(error);
-    }
+module.exports = function (monitor) {
+  let dataMonitor = monitor;
 
-    // use a standard Promise instead of async-await
-    // getClothingData()
-    //   .then(data => {
-    //     console.log("Returning clothing data");
-    //     res.send(data);
-    //   })
-    //   .catch(err => res.status(500).send(err))
-    //   .finally(() => console.log("All done processing the promise."));
-
-    console.log("Doing more work");
+  dataMonitor.on("dataAdded", (item) => {
+    setImmediate(() => console.log(`New data was added: ${item}`));
   });
 
-async function getClothingData() {
+  /* GET all clothing */
+  router
+    .route("/")
+    .get(async function (req, res) {
+      try {
+        let data = await getClothingData();
+        res.send(data);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    })
 
-  let rawData = await fsPromises.readFile(datafile, "utf8") // we can await only functions that return a promise
-  let clothingData = JSON.parse(rawData);
-  console.log(clothingData);
-  return clothingData;
+    .post(async function (req, res) {
+      try {
+        let data = await getClothingData();
 
-  // return new Promise((resolve, reject) => {
-  //   fs.readFile(datafile, "utf8", (err, data) => {
-  //     if (err) {
-  //       reject(err);
-  //     } else {
-  //       let clothingData = JSON.parse(data);
-  //       resolve(clothingData);
-  //     }
-  //   });
-  // });
-}
+        let nextID = getNextAvailableID(data);
 
-module.exports = router;
+        let newClothingItem = {
+          clothingID: nextID,
+          itemName: req.body.itemName,
+          price: req.body.price,
+        };
+
+        data.push(newClothingItem);
+
+        await saveClothingData(data);
+
+        dataMonitor.emit("dataAdded", newClothingItem.itemName);
+
+        console.log("Returning new item to browser.");
+
+        res.status(201).send(newClothingItem);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    });
+
+  async function getClothingData() {
+    let rawData = await fsPromises.readFile(datafile, "utf8");
+    let clothingData = JSON.parse(rawData);
+
+    return clothingData;
+  }
+
+  function getNextAvailableID(allClothingData) {
+    let maxID = 0;
+
+    allClothingData.forEach(function (element, index, array) {
+      if (element.clothingID > maxID) {
+        maxID = element.clothingID;
+      }
+    });
+    return ++maxID;
+  }
+
+  function saveClothingData(data) {
+    return fsPromises.writeFile(datafile, JSON.stringify(data, null, 4));
+  }
+
+  return router;
+};
