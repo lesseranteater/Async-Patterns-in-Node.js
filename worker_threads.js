b50c73d52320eb86edda39c69932438436aede2d
Tuama <lesser.anteater@gmail.com>
2021-12-22 06:03:18 +0200

    Worker thread example

--- worker_threads.js
+++ worker_threads.js
@@ -1,2 +1,7 @@
 const { Worker } = require("worker_threads");
 
+const firstWorker = new Worker("cpu_intensive.js");
+
+const secondWorker = new Worker(`console.log("Do CPU-intensive stuff here...")`,
+  { eval: true });
+
